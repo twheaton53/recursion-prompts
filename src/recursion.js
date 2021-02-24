adbf774a5b609dc027d9e23213d799b9356bda10
Travis Wheaton <traviswheaton53@gmail.com>
2021-02-23 17:33:42 -0800

    Implemented reverse recursion function

--- src/recursion.js
+++ src/recursion.js
@@ -156,6 +156,12 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  // Base case is when when string is empty
+  if (string === '') {
+    return '';
+  } else {
+    return reverse(string.substr(1)) + string.charAt(0);
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
