6afeb64546fbe8f9668cdefaa6fad4347b20690c
Travis Wheaton <traviswheaton53@gmail.com>
2021-02-23 17:08:26 -0800

    Implemented exponent recursion function

--- src/recursion.js
+++ src/recursion.js
@@ -124,6 +124,20 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  // Edge case of exponent being zero
+  if (exp === 0) {
+    return 1;
+  }
+  // Base Case is when exponent is 1
+  if (exp === 1) {
+    return base;
+  }
+
+  if (exp < 0) {
+    return 1 / (exponent(base, (exp * -1)));
+  }
+
+  return base * exponent(base, exp - 1);
 };
 
 // 8. Determine if a number is a power of two.
